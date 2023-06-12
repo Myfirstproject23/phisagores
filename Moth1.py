1286f4227accf4018baf1ae8798cec65861b6d9b
Mster__Sycna_AI <128229109+Myfirstproject23@users.noreply.github.com>
2023-06-12 15:02:03 +0330

    Add files via upload

---
+++ Moth1.py
@@ -0,0 +1,23 @@
+import math
+
+b = int(input('Enter Your name b\n'))
+c = int(input('Enter Your name c\n'))
+
+def show_answer():
+    Vatar= ('a')
+    sqrt_num = int(math.sqrt(answer))
+    print("answer your , " + str(Vatar) + " " + str(sqrt_num) + "")
+
+Pythagoras = b + c
+power = b ** 2
+power2 = c ** 2
+answer = power + power2
+print(answer)
+
+num = answer
+sqrt_num = int(math.sqrt(num))
+
+print("Square root of", num, "is", sqrt_num)
+
+show_answer()
+ 
